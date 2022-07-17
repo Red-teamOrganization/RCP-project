52fb5a7d4d66ecdc450870662b50623f67346918
dionisis-deligiannis <delisdionysios@gmail.com>
2022-07-17 11:32:09 +0300

    footer fix

--- rcp/src/pages/Signup.js
+++ rcp/src/pages/Signup.js
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { LockClosedIcon } from "@heroicons/react/solid";
 import LoadingComponent from "../components/LoadingComponent";
 import logo from "../images/logo.png";
+import "./Footer.css"
 
 export default function Signup() {
   const [data, setData] = useState({
@@ -66,7 +67,7 @@ export default function Signup() {
 
   return (
     <>
-      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="min-h-full signup-login-wrapper flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
             <img src={logo} width={"150px"} className="mx-auto" alt="" />
