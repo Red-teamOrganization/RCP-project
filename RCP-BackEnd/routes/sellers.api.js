4069dcc90d6c8a2dcdfb6c12fd605953bd0b46f7
zikaloai71 <Ze.Lo.Moukhtar@el-eng.menofia.edu.eg>
2022-10-12 21:16:16 +0200

    seller can add logo to his profile

--- RCP-BackEnd/routes/sellers.api.js
+++ RCP-BackEnd/routes/sellers.api.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const sellerController = require("../app/controllers/seller.controller");
 const sellerAuth = require("../app/middleware/sellerAuth")
-
+const upload = require("../app/middleware/file.upload");
 router.post("/signUp", sellerController.signUp);
 
 router.post("/logIn", sellerController.logIn);
@@ -9,7 +9,12 @@ router.post("/logIn", sellerController.logIn);
 router.get("/logOut",sellerAuth, sellerController.logOut);
 
 router.get("/sellerProfile",sellerAuth, sellerController.sellerProfile);
-
+router.post(
+    "/sellerProfile/sellerLogo",
+    sellerAuth,
+    upload.single("logo"),
+    sellerController.sellerLogoUpload
+  );
 router.get("/logOutAll",sellerAuth, sellerController.logOutAll);
 
 
