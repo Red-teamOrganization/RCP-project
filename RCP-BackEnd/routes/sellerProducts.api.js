948ee299e4455562440f664c6e4bfe86c7912e75
zikaloai71 <Ze.Lo.Moukhtar@el-eng.menofia.edu.eg>
2022-10-13 21:44:25 +0200

    fetch the data of whole market for sellers based on categories api added

--- RCP-BackEnd/routes/sellerProducts.api.js
+++ RCP-BackEnd/routes/sellerProducts.api.js
@@ -4,6 +4,8 @@ const sellerAuth = require("../app/middleware/sellerAuth")
 
 router.post("/addSoldProduct",sellerAuth, soldProductsController.addProduct);
 router.get("/mySoldProducts",sellerAuth, soldProductsController.mySoldProducts);
-
+router.get("/allSellerAgricultureProducts",soldProductsController.totalAgricultureSoldProducts)
+router.get("/allSellerDiaryProducts",soldProductsController.totalDiarySoldProducts)
+router.get("/allSellerProteinProducts",soldProductsController.totalProteinSoldProducts)
 
 module.exports = router;
